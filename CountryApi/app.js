cdb04118482be9aa5831c41cc29287a82b201270
HuseynliTogrul <toghrulih@code.edu.az>
2024-01-10 03:26:28 +0400

    CountryApi task end

--- CountryApi/app.js
+++ CountryApi/app.js
@@ -1,97 +1,90 @@
 "use strict";
 
-const countries = [
-    {
-        name: "Germany",
-        capital: "Berlin",
-        region: "Europe",
-        population: 83240525,
-        flag: "https://flagcdn.com/sc.svg"
-    },
-    {
-        name: "United States of America",
-        capital: "Washington, D.C.",
-        region: "Americas",
-        population: 329484123,
-        flag: "https://flagcdn.com/us.svg"
-    },
-    {
-        name: "Brazil",
-        capital: "Brasília",
-        region: "Americas",
-        population: 212559409,
-        flag: "https://flagcdn.com/br.svg"
-    },
-    {
-        name: "Iceland",
-        capital: "Reykjavík",
-        region: "Europe",
-        population: 366425,
-        flag: "https://flagcdn.com/is.svg"
-    },
-    {
-        name: "Afghanistan",
-        capital: "Kabul",
-        region: "Asia",
-        population: 40218234,
-        flag: "https://upload.wikimedia.org/wikipedia/commons/5/5c/Flag_of_the_Taliban.svg"
-    },
-    {
-        name: "Åland Islands",
-        capital: "Mariehamn",
-        region: "Europe",
-        population: 28875,
-        flag: "https://flagcdn.com/sm.svg"
-    },
-    {
-        name: "Albania",
-        capital: "Tirana",
-        region: "Europe",
-        population: 2837743,
-        flag: "https://flagcdn.com/al.svg"
-    },
-    {
-        name: "Algeria",
-        capital: "Algiers",
-        region: "Africa",
-        population: 44700000,
-        flag: "https://flagcdn.com/dz.svg"
-    }
-]
-
 const headerRight = document.querySelector(".header-right")
-// const worldHeader = document.querySelector(".world-header")
 const inputLeft = document.querySelector(".input-left input")
 const worldFlags = document.querySelector(".world-flags")
 
-const element = document.body;
+const filterBtn = document.querySelector(".filterBtn")
+const filterChevron = document.querySelector(".filterBtn i")
+const filterList = document.querySelector(".filterList")
 
-headerRight.addEventListener("click", () => {
-    element.classList.toggle("dark-mode");
-    // worldHeader.classList.toggle("dark-mode");
+const filterRegionList = document.querySelectorAll(".filterList li")
+
+const searchInput = document.querySelector("#searchInput")
+
+let countries;
+
+filterBtn.addEventListener("click", () => {
+    // filterChevron.style.transform = "rotate(180deg)"
+    filterList.classList.toggle("hidden")
 })
 
-inputLeft.addEventListener("input", () => {
-    const givenValue = inputLeft.value.trim()
-    console.log(givenValue);
+// headerRight.addEventListener("click", () => {
+//     html.classList.toggle("dark");
+// })
 
+const getCountries = async () => {
+    const response = await fetch(`https://restcountries.com/v3.1/all`)
+    const data = await response.json()
+
+    countries = data
+    displayCountries(countries)
+}
+
+getCountries()
+
+const displayCountries = (countries) => {
+    worldFlags.innerHTML = ""
+    countries.forEach((country) => {
+
+        if (country.name.common === "Armenia") {
+            return
+        }
+
+        worldFlags.innerHTML += `<div class="world">
+                        <div class= "world-img">
+                            <img src="${country.flags.png}" alt="">
+                        </div>
+                        <div class="world-info-name">
+                            <div class="world-name">
+                                ${country.name.common}
+                            </div>
+                            <div class="world-info">
+                                <ul>
+                                    <li>Population: <span>${country.population}</span></li>
+                                    <li>Region: <span>${country.region}</span></li>
+                                    <li>Capital: <span>${country.capital ? country.capital : 'Not defined'}</span></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>`
+    })
+
+}
+
+const searchCountry = () => {
+    const givenValue = searchInput.value.trim().toLowerCase();
+    const filteredCountries = countries.filter((country) =>
+        country.name.common.toLowerCase().includes(givenValue)
+    )
+
+    displayCountries(filteredCountries)
+    searchInput.value = ""
+}
+
+searchInput.addEventListener("keydown", (event) => {
+    if (event.code === "Enter") {
+        searchCountry()
+    }
+})
+
+filterRegionList.forEach((regionList) => {
+    regionList.addEventListener("click", () => {
+        const { region } = regionList.dataset;
+        filterList.classList.add("hidden")
+        console.log(region);
+        const filteredRegion = countries.filter((country) => country.region === region)
+
+        displayCountries(filteredRegion)
+    })
 });
-countries.forEach((country) => {
-    worldFlags.innerHTML += `<div class="world">
-                    <div class= "world-img">
-                        <img src="${country.flag}" alt="">
-                    </div>
-                    <div class="world-info-name">
-                        <div class="world-name">
-                            ${country.name}
-                        </div>
-                        <div class="world-info">
-                            <ul>
-                                <li>Population: <span>${country.population}</span></li>
-                                <li>Region: <span>${country.region}</span></li>
-                                <li>Capital: <span>${country.capital}</span></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>`
-})
